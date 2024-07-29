f12baca0fd0bcd96416f89223221dbed2a752114
jholl1886 <jacobholleman1886@gmail.com>
2024-07-29 14:28:02 -0400

    Recipe.java done

--- app/src/main/java/com/zybooks/recipeshare/repo/RecipeDao.java
+++ app/src/main/java/com/zybooks/recipeshare/repo/RecipeDao.java
@@ -5,5 +5,23 @@ import java.util.List;
 import com.zybooks.recipeshare.model.Recipe;
 @Dao
 public interface RecipeDao  {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(Recipe recipe);
+
+    @Update
+    void update(Recipe recipe);
+
+    @Delete
+    void delete(Recipe recipe);
+
+    @Query("SELECT * FROM Recipes")
+    LiveData<List<Recipe>> getAllRecipes();
+
+    @Query("SELECT * FROM Recipes WHERE recipe_id = :id")
+    LiveData<Recipe> getRecipeById(int id);
+
+    @Query("SELECT * FROM Recipes WHERE name LIKE :name")
+    LiveData<List<Recipe>> getRecipesByName(String name);
+
 
 }
