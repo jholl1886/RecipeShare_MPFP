13ab8fad5f971b72567c59dfffd801ad7009edbc
jholl1886 <jacobholleman1886@gmail.com>
2024-07-29 13:49:43 -0400

    slowly adding all the data base stuff

---
+++ app/src/main/java/com/zybooks/recipeshare/repo/RecipeDao.java
@@ -0,0 +1,9 @@
+package com.zybooks.recipeshare.repo;
+import androidx.lifecycle.LiveData;
+import androidx.room.*;
+import java.util.List;
+import com.zybooks.recipeshare.model.Recipe;
+@Dao
+public interface RecipeDao  {
+
+}
